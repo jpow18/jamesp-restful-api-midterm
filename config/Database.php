8c1e1aecfdb8d589b59a9b618d424b0ee51a7d14
James Pow <jbpow18@icloud.com>
2023-02-28 21:20:55 -0500

    feat/add-connect-method-to-database-class

--- config/Database.php
+++ config/Database.php
@@ -15,6 +15,24 @@
       $this->host = getenv('HOST');
       $this->port = getenv('PORT');
     }
+
+    public function connect() {
+      // Check if connection already exists
+      if ($this->conn) {
+        // connection already exists, return it
+        return $this->conn;
+      } else {
+        $dsn = "pgsql:host={$this->host};port={$this->port};" . 
+        "dbname={$this->dbname};";
+
+        try {
+          $this->conn = new PDO($dsn, $this->username, $this->password);
+          $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+          echo 'Connection Error: ' . $e->getMessage();
+        }
+      }
+    }
   }
 
 ?>
