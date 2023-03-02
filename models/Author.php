eb40699d46db08e84ebf9c0a81df891da1cbdc55
James Pow <jbpow18@icloud.com>
2023-03-02 18:50:42 -0500

    feat/add-read-method

---
+++ models/Author.php
@@ -0,0 +1,32 @@
+<?php
+
+  class Author {
+    // DB stuff
+    private $conn;
+    private $table = 'authors';
+
+    // Author Properties
+    public $id;
+    public $author;
+
+    // Constructor with DB
+    public function __construct($db) {
+      $this->conn = $db;
+    }
+
+    // Get Authors
+    public function read() {
+      // Create query
+      $query = 'SELECT * FROM ' . $this->table;
+
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Execute query
+      $stmt->execute();
+
+      return $stmt;
+    }
+  }
+
+?>
