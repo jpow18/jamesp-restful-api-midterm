0e8628271a0b7caeeceac575f6beac987a23a9ee
James Pow <jbpow18@icloud.com>
2023-03-03 13:15:45 -0500

    feat/added-necessary-code-to-create-author

---
+++ api/authors/create.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once "../../config/Database.php";
+include_once "../../models/Author.php";
+
+// Instantiate DB and connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate Author object
+$author = new Author($db);
+
+// Get raw posted data
+$data = json_decode(file_get_contents("php://input"));
+
+$author->author = $data->author;
+
+// Create author
+if($author->create()) {
+  echo 'created author (' . $author->id . ', ' . $author->author . ')';
+} else {
+  echo json_encode(
+    array('message' => 'author_id not found')
+  );
+}
+
+?>
