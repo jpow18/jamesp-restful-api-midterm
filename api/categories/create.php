1062aa0c63f4fec1eef51a53b7258d32319192e6
James Pow <jbpow18@icloud.com>
2023-03-06 09:56:32 -0500

    feat/copied-from-authors-changed-references

---
+++ api/categories/create.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once "../../config/Database.php";
+include_once "../../models/Category.php";
+
+// Instantiate DB and connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate Category object
+$category = new Category($db);
+
+// Get raw posted data
+$data = json_decode(file_get_contents("php://input"));
+
+// Check if 'category' parameter exists in the JSON data
+if (!$data || !isset($data->category)) {
+  echo json_encode(
+    array('message' => 'Missing Required Parameters')
+  );
+} else {
+  $category->category = $data->category;
+  // Create category
+  if($category->create()) {
+    echo 'created category (' . $category->id . ', ' . $category->category . ')';
+  } else {
+    echo json_encode(
+      array('message' => 'category_id not found')
+    );
+  }
+}
+?>
