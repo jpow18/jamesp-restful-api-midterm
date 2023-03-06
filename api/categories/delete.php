0dd9440ca240cd33d8d97035a9a8590c5d09d897
James Pow <jbpow18@icloud.com>
2023-03-06 09:57:24 -0500

    feat/copied-from-authors-changed-references

---
+++ api/categories/delete.php
@@ -0,0 +1,32 @@
+<?php
+
+  include_once "../../config/Database.php";
+  include_once "../../models/Category.php";
+
+  // Instantiate DB and connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate Category object
+  $category = new Category($db);
+
+  // Get raw posted data
+  $data = json_decode(file_get_contents("php://input"));
+
+  // Check if 'category' and 'id' parameters exist in the JSON data
+  if (!$data || !isset($data->id)) {
+    echo json_encode(
+      array('message' => 'Missing Required Parameters')
+    );
+  } else {
+    $category->id = $data->id;
+    // Delete category
+    if ($category->delete()) {
+      echo $category->id . ' of deleted category';
+    } else {
+      echo json_encode(
+        array('message' => 'category_id not found')
+      );
+    }
+  }
+?>
