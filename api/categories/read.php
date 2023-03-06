50744c0e1db7db26c9183c3d49562c5e308c9436
James Pow <jbpow18@icloud.com>
2023-03-06 10:18:03 -0500

    fix/corrected-syntax

--- api/categories/read.php
+++ api/categories/read.php
@@ -8,34 +8,34 @@
   $db = $database->connect();
 
   // Instantiate Category object
-  $categories = new Category($db);
+  $category = new Category($db);
 
   // Read Category query
-  $result = $categories->read();
+  $result = $category->read();
   // Get row count
   $num = $result->rowCount();
 
   // check if any categoriess
   if ($num > 0) {
     // Category array
-    $categories_arr = array();
+    $category_arr = array();
 
     while($row = $result->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
 
-        $categories_item = array(
+        $category_item = array(
           'id' => $id,
-          'categories' => $categories
+          'category' => $category
         );
 
         // Push to "data"
-        array_push($categories_arr, $categories_item);
+        array_push($category_arr, $category_item);
       }
       // Turn to JSON and output
-      echo json_encode($categories_arr);
+      echo json_encode($category_arr);
   } else {
     // No Categories
     echo json_encode(
-      array('message' => 'categories_id Not Found')
+      array('message' => 'category_id Not Found')
     );
   }
