0f54a107283be0de1cdbd7043855153b28b8caee
James Pow <jbpow18@icloud.com>
2023-03-07 19:39:21 -0500

    feat/defined-read_single-method

--- models/Quote.php
+++ models/Quote.php
@@ -33,5 +33,35 @@
 
       return $stmt;
     }
+
+    // Get single quote
+    public function read_single() {
+      // Create query
+      $query = 'SELECT q.id, q.quote, a.author as author_name,
+        c.category as category_name 
+          FROM quotes q 
+          LEFT JOIN authors a ON q.author_id = a.id 
+          LEFT JOIN categories c ON q.category_id = c.id
+          WHERE q.id = :id';
+
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Bind ID
+      $stmt->bindParam(1, $this->id);
+
+      // Execute query
+      $stmt->execute();
+
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      if ($row > 0) {
+        // Set properties
+        $this->id = $row['id'];
+        $this->quote = $row['quote'];
+        $this->author_id = $row['author_name'];
+        $this->category_id = $row['category_name'];
+      }
+    }
   }
 ?>
