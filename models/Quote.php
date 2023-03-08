5635bed38c89556a7a7bc3e08ae0d850d2f00130
James Pow <jbpow18@icloud.com>
2023-03-08 11:52:15 -0500

    feat/defined-create-method

--- models/Quote.php
+++ models/Quote.php
@@ -63,5 +63,37 @@
         $this->category_id = $row['category_name'];
       }
     }
+
+    // Create new quote
+    public function create() {
+      // query
+      $query = 'INSERT INTO ' . $this->table . ' (quote, author_id, category_id)
+          VALUES (:quote, :author_id, :category_id) RETURNING id';
+
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Clean data
+      $this->quote = htmlspecialchars(strip_tags($this->quote));
+
+      // Bind data
+      $stmt->bindParam(':quote', $this->quote);
+      $stmt->bindParam(':author_id', $this->author_id);
+      $stmt->bindParam(':category_id', $this->category_id);
+
+      // Execute query
+      if ($stmt->execute()) {
+        // Get ID of the newly created quote
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $this->id = $row['id'];
+        return true;
+      }
+
+      // Print error if something goes wrong
+      printf("Error: %s.\n", $stmt->error);
+
+      return false;
+    }
   }
+  
 ?>
