8b5f23e94a06ff3bc2219df9d91211d8d92fd140
James Pow <jbpow18@icloud.com>
2023-03-10 12:20:00 -0500

    fix/added-conditional-to-deal-with-invalid-category-and-author-ids

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -24,19 +24,37 @@ if (!$data || !isset($data->quote) || !isset($data->id)
   $quote->author_id = $data->author_id;
   $quote->category_id = $data->category_id;
   $quote->id = $data->id;
-  // Update quote
-  if ($quote->update()) {
-    echo json_encode(
-      array(
-        'id' => $quote->id,
-        'quote' => $quote->quote,
-        'author_id' => $quote->author_id,
-        'category_id' => $quote->category_id
-      )
-    );
-  } else {
-    echo json_encode(
-      array('message' => 'quote_id not found')
-    );
+  // Create quote
+  try {
+    if ($quote->create()) {
+      echo json_encode(
+        array(
+          'id' => $quote->id,
+          'quote' => $quote->quote,
+          'author_id' => $quote->author_id,
+          'category_id' => $quote->category_id
+        )
+      );
+    }
+  } catch (PDOException $e) {
+    if ($e->getCode() == '23503') {
+      // Get the key value that caused the error
+      $key_value = substr($e->getMessage(), strpos($e->getMessage(), '(') + 1, strpos($e->getMessage(), ')') - strpos($e->getMessage(), '(') - 1);
+
+      // check if author_id or category_id is not found
+      if ($key_value === 'author_id') {
+        echo json_encode(
+          array('message' => 'author_id Not Found')
+        );
+      } else {
+        echo json_encode(
+          array('message' => 'category_id Not Found')
+        );
+      }
+    } else {
+      echo json_encode(
+        array('message' => 'No Quotes Found')
+      );
+    }
   }
 }
